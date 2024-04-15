0cde67a5261d2bae3d548c89ec62c89f53dc0d82
Arfaxad Zadot <fantasma1940@gmail.com>
2024-04-15 09:51:12 -0600

    Tema de Cors

--- SapaspaProject/settings.py
+++ SapaspaProject/settings.py
@@ -28,12 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = (
+""" CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000'
-)
+) """
 # Application definition
 
 INSTALLED_APPS = [
